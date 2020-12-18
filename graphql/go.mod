02e730ea36fae075d04d4167df3016f8e0499609
DucNH2 <ducnh2@fabbi.io>
2020-12-18 14:34:48 +0700

    Add Gateway for product service

--- graphql/go.mod
+++ graphql/go.mod
@@ -4,15 +4,11 @@ go 1.15
 
 require (
 	github.com/99designs/gqlgen v0.13.0
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/golang/protobuf v1.4.3
 	github.com/joho/godotenv v1.3.0
 	github.com/sirupsen/logrus v1.7.0
 	github.com/vektah/gqlparser/v2 v2.1.0
-	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
 	google.golang.org/grpc v1.34.0
 	google.golang.org/protobuf v1.25.0
-	gorm.io/driver/mysql v1.0.3 // indirect
-	gorm.io/gorm v1.20.8 // indirect
 )
--- graphql/graph/resolver/products.resolvers.go
+++ graphql/graph/resolver/products.resolvers.go
@@ -7,25 +7,153 @@ import (
 	"context"
 	"example/graph/custom_model"
 	"example/graph/graph_model"
+	productService "example/grpc/product"
 	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
 func (r *mutationResolver) ProductCreate(ctx context.Context, input graph_model.CreateProduct) (*graph_model.Product, error) {
-	panic(fmt.Errorf("not implemented"))
+	connection, err := grpc.Dial(os.Getenv("PRODUCT_GRPC_SERVER"), grpc.WithInsecure())
+	if err != nil {
+		log.Errorf("Did not connect to product server: %v", err)
+		return &graph_model.Product{}, err
+	}
+	defer connection.Close()
+	client := productService.NewProductServiceClient(connection)
+	result, err := client.CreateProduct(ctx, &productService.CreateProductRequest{
+		ProductCode: input.ProductCode,
+		ProductName: input.ProductName,
+		Price:       int64(input.Price),
+	})
+	if err != nil {
+		log.Error(err)
+		errorDetail, _ := status.FromError(err)
+		return &graph_model.Product{}, fmt.Errorf("%s", errorDetail.Message())
+	}
+	id := int(result.Id)
+	price := int(result.Price)
+	return &graph_model.Product{
+		ID:          &id,
+		ProductCode: &result.ProductCode,
+		ProductName: &result.ProductName,
+		Price:       &price,
+		CreatedAt:   &result.CreatedAt,
+		UpdatedAt:   &result.UpdatedAt,
+	}, nil
 }
 
 func (r *mutationResolver) ProductUpdate(ctx context.Context, input custom_model.UpdateProduct) (*graph_model.Product, error) {
-	panic(fmt.Errorf("not implemented"))
+	connection, err := grpc.Dial(os.Getenv("PRODUCT_GRPC_SERVER"), grpc.WithInsecure())
+	if err != nil {
+		log.Errorf("Did not connect to product server: %v", err)
+		return &graph_model.Product{}, err
+	}
+	defer connection.Close()
+	client := productService.NewProductServiceClient(connection)
+	result, err := client.UpdateProduct(ctx, &productService.UpdateProductRequest{
+		Id:          int32(input.ID),
+		ProductCode: input.ProductCode,
+		ProductName: input.ProductName,
+		Price:       int64(input.Price),
+	})
+	if err != nil {
+		log.Error(err)
+		errorDetail, _ := status.FromError(err)
+		return &graph_model.Product{}, fmt.Errorf("%s", errorDetail.Message())
+	}
+	id := int(result.Id)
+	price := int(result.Price)
+	return &graph_model.Product{
+		ID:          &id,
+		ProductCode: &result.ProductCode,
+		ProductName: &result.ProductName,
+		Price:       &price,
+		CreatedAt:   &result.CreatedAt,
+		UpdatedAt:   &result.UpdatedAt,
+	}, nil
 }
 
 func (r *mutationResolver) ProductDelete(ctx context.Context, input graph_model.DeleteProduct) (*int, error) {
-	panic(fmt.Errorf("not implemented"))
+	connection, err := grpc.Dial(os.Getenv("PRODUCT_GRPC_SERVER"), grpc.WithInsecure())
+	if err != nil {
+		log.Errorf("Did not connect to product server: %v", err)
+		return nil, err
+	}
+	defer connection.Close()
+	client := productService.NewProductServiceClient(connection)
+	_, err = client.DeleteProduct(ctx, &productService.DeleteProductRequest{
+		Id: int32(input.ID),
+	})
+	if err != nil {
+		log.Error(err)
+		errorDetail, _ := status.FromError(err)
+		return nil, fmt.Errorf("%s", errorDetail.Message())
+	}
+	return &input.ID, nil
 }
 
 func (r *queryResolver) Product(ctx context.Context, id int) (*graph_model.Product, error) {
-	panic(fmt.Errorf("not implemented"))
+	connection, err := grpc.Dial(os.Getenv("PRODUCT_GRPC_SERVER"), grpc.WithInsecure())
+	if err != nil {
+		log.Errorf("Did not connect to product server: %v", err)
+		return &graph_model.Product{}, err
+	}
+	defer connection.Close()
+	client := productService.NewProductServiceClient(connection)
+	result, err := client.GetProduct(ctx, &productService.GetProductRequest{
+		Id: int32(id),
+	})
+	if err != nil {
+		log.Error(err)
+		errorDetail, _ := status.FromError(err)
+		return &graph_model.Product{}, fmt.Errorf("%s", errorDetail.Message())
+	}
+	productId := int(result.Id)
+	price := int(result.Price)
+	return &graph_model.Product{
+		ID:          &productId,
+		ProductCode: &result.ProductCode,
+		ProductName: &result.ProductName,
+		Price:       &price,
+		CreatedAt:   &result.CreatedAt,
+		UpdatedAt:   &result.UpdatedAt,
+	}, nil
 }
 
 func (r *queryResolver) Products(ctx context.Context, filter string, limit int, page int) ([]graph_model.Product, error) {
-	panic(fmt.Errorf("not implemented"))
+	connection, err := grpc.Dial(os.Getenv("PRODUCT_GRPC_SERVER"), grpc.WithInsecure())
+	if err != nil {
+		log.Errorf("Did not connect to product server: %v", err)
+		return []graph_model.Product{}, err
+	}
+	defer connection.Close()
+	client := productService.NewProductServiceClient(connection)
+	result, err := client.GetListProduct(ctx, &productService.GetListProductRequest{
+		Limit: int32(limit),
+		Page:  int32(page),
+	})
+	if err != nil {
+		log.Error(err)
+		errorDetail, _ := status.FromError(err)
+		return []graph_model.Product{}, fmt.Errorf("%s", errorDetail.Message())
+	}
+	listProduct := []graph_model.Product{}
+	for _, product := range result.ListProduct {
+		id := int(product.Id)
+		price := int(product.Price)
+		listProduct = append(listProduct, graph_model.Product{
+			ID:          &id,
+			ProductCode: &product.ProductCode,
+			ProductName: &product.ProductName,
+			Price:       &price,
+			CreatedAt:   &product.CreatedAt,
+			UpdatedAt:   &product.UpdatedAt,
+		})
+	}
+	return listProduct, nil
 }
